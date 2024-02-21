bed7da697e2e00b2bbe77b9246a8ac0583627dea
Vojtech Lukes <vojtech.lukes@tul.cz>
2024-02-21 17:22:34 +0100

    Point add from school

--- src/lukes/PointApp.java
+++ src/lukes/PointApp.java
@@ -1,15 +1,17 @@
 package lukes;
+
 public class PointApp {
-        public static void main(String[] args) {
-        Point bod1=new Point(2,4);
+
+    public static void main(String[] args) {
+        Point bod1 = new Point(2, 4);
         System.out.println(bod1.getDistanceFromOrigin());
-        Point bod2=new Point(5,6);
+        Point bod2 = new Point(5, 6);
         bod1.distanceFrom(bod2);
         bod2.distanceFrom(bod1);
-            System.out.println(bod1.distanceFrom(bod2));
-            System.out.println(bod2.distanceFrom(bod1));
-            System.out.println(bod1);
-            System.out.println(PointTools.distanceFrom(bod1, bod2));
-           
+        System.out.println(bod1.distanceFrom(bod2));
+        System.out.println(bod2.distanceFrom(bod1));
+        System.out.println(bod1);
+        System.out.println(PointTools.distanceFrom(bod1, bod2));
+
     }
 }
--- src/lukes/PointTools.java
+++ src/lukes/PointTools.java
@@ -1,10 +1,13 @@
 package lukes;
+
 public class PointTools {
-    private PointTools(){
+
+    private PointTools() {
         //znemozni vytvareni objektu tridy PointTools
     }
-       public static double distanceFrom(Point a, Point b){
-       return Math.hypot(a.getX()-b.getX(), a.getY()-b.getY());
-       //obecne nepouzivame static metody
-   }
+
+    public static double distanceFrom(Point a, Point b) {
+        return Math.hypot(a.getX() - b.getX(), a.getY() - b.getY());
+        //obecne nepouzivame static metody
+    }
 }
